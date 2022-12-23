3718c9b5978ce79729002820cd6c1359a765bec4
TNC9 <koonbenzkub@hotmail.com>
2022-12-23 15:33:24 +0700

    Complete

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -4,6 +4,19 @@
 using namespace std;
 
 //[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(double grade){
+    if(grade > 90)
+        grade = 'A';
+    else if(grade > 75 && grade <= 90)
+        grade = 'B';
+    else if(grade > 60 && grade <= 75)
+        grade = 'C';
+    else if(grade > 45 && grade <= 60)
+        grade = 'D';
+    else if(grade <= 45)
+        grade = 'F';
+    return grade;
+}
 
 int main(){
 	//Input the number of students
@@ -18,8 +31,10 @@ int main(){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
 		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin,name[i]);
 		cout << "Score of student " << i+1 << ": ";
 		//[Missing Code 3] Get score of the i-th students.
+		cin >> score[i];
 		i++;
 	}
 	
